001e661458e74ef3daa75b3eeee31fe8a254c251
kotaiahandraju <a.kotaiah12@gmail.com>
2018-07-02 15:44:42 +0530

    changes in admin Controller

--- src/main/java/com/charvikent/sample/controller/adminController.java
+++ src/main/java/com/charvikent/sample/controller/adminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class adminController {
 	
-	/** this is coding for error message
+	/** this is coding for error messages
 	 * @return
 	 */
 	@GetMapping("*")
